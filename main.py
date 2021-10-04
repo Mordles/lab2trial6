ef91b2c736c9469cb4dee8e279b1e52b14e1d15a
Mordles <91845645+Mordles@users.noreply.github.com>
2021-10-04 14:40:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-btn_input = 4;
-LED_output = 26;
+btn_input = 20;
+LED_output = 21;
 
 # GPIO btn_input set up as input.
 GPIO.setup(btn_input, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(LED_output, GPIO.OUT, initial=0)
+GPIO.setup(LED_output, GPIO.OUT)
 
 # handle the button event
 def buttonEventHandler_rising (pin):
@@ -20,8 +20,8 @@ def buttonEventHandler_falling (pin):
 
 
 	
-GPIO.add_event_detect(btn_input, GPIO.RISING, callback=buttonEventHandler_rising) 
-GPIO.add_event_detect(btn_input, GPIO.FALLING, callback=buttonEventHandler_falling)
+GPIO.add_event_detect(btn_input, GPIO.RISING, callback=buttonEventHandler_rising, bouncetime=100) 
+GPIO.add_event_detect(btn_input, GPIO.FALLING, callback=buttonEventHandler_falling, bouncetime=100)
  
 try:  
     while True : pass  
